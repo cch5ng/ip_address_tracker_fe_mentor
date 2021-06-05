dfd17aa74993606278ce2c56e270c8fb8436295c
Carol Chung <cch5ng@gmail.com>
2021-06-04 19:47:47 -0700

    global styles; refactor styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'desktop': '1440px',
+      'desktop': '1200px',
       // => @media (min-width: 1440px) { ... }
+    },
+    colors: {
+      grey: {
+        dark: 'hsl(0, 0%, 59%)',
+        darkest: 'hsl(0, 0%, 17%)',
+      }
+    },
+    minHeight: {
+      '1/4': '25%',
+      '1/3': '33%',
     },
     extend: {},
   },
