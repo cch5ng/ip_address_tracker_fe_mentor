170ad44b273a9331278052ab83cacdf81a700557
Carol Chung <cch5ng@gmail.com>
2021-06-06 19:13:23 -0700

    3 style (#8)
    * global style
    
    * global styles; refactor styles
    
    * add styling to the form elements and top section
    
    * style the ip data section
    
    * add logic to prepopulate map
    
    * move images to public dir
    
    * move images to public
    
    * configure background image
    
    * set background image
    
    * add missing colors
    
    * fix layout for the search field
    
    * add marker to map
    
    * add input change handler and manage ipify request

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,30 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'desktop': '1440px',
+      'desktop': '1200px',
       // => @media (min-width: 1440px) { ... }
     },
-    extend: {},
+    colors: {
+      grey: {
+        dark: 'hsl(0, 0%, 59%)',
+        darkest: 'hsl(0, 0%, 17%)',
+      },
+      black: {
+        DEFAULT: '#000',
+      },
+      white: {
+        DEFAULT: '#fff',
+      },
+    },
+    minHeight: {
+      '1/4': '25%',
+      '1/3': '33%',
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'hero-pattern': "url('../public/pattern-bg.png')",
+      }),
+    },
   },
   variants: {
     extend: {},
